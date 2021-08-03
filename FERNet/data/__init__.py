3d394af8bfd677ea378cd872dffd4f367d97adec
RobotBj <88359547+RobotBj@users.noreply.github.com>
2021-08-03 15:26:45 +0800

    Add files via upload

---
+++ FERNet/data/__init__.py
@@ -0,0 +1,5 @@
+from .VOCloading import VOCDetection, AnnotationTransform, detection_collate, VOC_CLASSES, URPCDetection,\
+    URPCAnnotationTransform
+from .coco import COCODetection
+from .data_augment import *
+from .config import *
