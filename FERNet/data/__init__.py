a15aca5f52dce8410ab03f7e06b9e2616a919063
RobotBj <88359547+RobotBj@users.noreply.github.com>
2021-08-03 15:30:49 +0800

    Delete FERNet directory

--- FERNet/data/__init__.py
+++
@@ -1,5 +0,0 @@
-from .VOCloading import VOCDetection, AnnotationTransform, detection_collate, VOC_CLASSES, URPCDetection,\
-    URPCAnnotationTransform
-from .coco import COCODetection
-from .data_augment import *
-from .config import *
